1d8c118712895d9dfb9aa2acd65f39c421eb02e6
Sojeong Lee <162033491+Sojeong0430@users.noreply.github.com>
2025-04-09 22:10:05 +0900

    ✅ ch07 practice

--- study/src/main/java/com/example/study/MainActivity.kt
+++ study/src/main/java/com/example/study/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.study
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.study.databinding.ActivityMainBinding
 
@@ -8,14 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //바인딩 객체 획득
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        //액티비티 화면 출력
         setContentView(binding.root)
 
-        //뷰 객체 이용
-        binding.visibleBtn.setOnClickListener{
-            binding.targetView.visibility
+        binding.button.setOnClickListener{
+            binding.imageView.visibility = View.VISIBLE
+            binding.button.visibility = View.INVISIBLE
+        }
+
+        binding.imageView.setOnClickListener(){
+            binding.button.visibility = View.VISIBLE
+            binding.imageView.visibility = View.INVISIBLE
         }
 
     }
